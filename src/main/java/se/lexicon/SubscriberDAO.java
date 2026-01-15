fb9391221d10c0cd9c17d82671d3b1fc0ff11425
ornella28 <angelnibaruta88@gmail.com>
2026-01-15 14:04:35 +0100

    creating SubscriberDAO

--- src/main/java/se/lexicon/SubscriberDAO.java
+++ src/main/java/se/lexicon/SubscriberDAO.java
@@ -1,4 +1,18 @@
 package se.lexicon;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SubscriberDAO {
+
+    //Creating a private list to store subscribers
+    private final List<Subscriber> storeSubscribers = new ArrayList<>();
+
+    //Creating a method to save subscribers to the list
+    public void save(Subscriber subscriber){
+        storeSubscribers.add(subscriber);
+    }
+
+
+
 }
